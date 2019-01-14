b0acc319b9e1bad148ecf0b8de99e4fa440dc8c2
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-01-14 02:47:40 -0600

    Updates

---
+++ precache-manifest.2e5c9d1b6513ca0ecfd4a2c86af6c3d2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb1bde2e41ace79525752131b639ff93",
+    "url": "/clicky-game/static/media/S4e14_just_Finn.bb1bde2e.png"
+  },
+  {
+    "revision": "51f8efeff91d3e1cf36f",
+    "url": "/clicky-game/static/css/main.eed8e0fb.chunk.css"
+  },
+  {
+    "revision": "0a00d6494d13a481a982",
+    "url": "/clicky-game/static/js/1.0a00d649.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "d77f46ab89da8e73904810c0ad32dada",
+    "url": "/clicky-game/static/media/adventureTimeHeader.d77f46ab.jpg"
+  },
+  {
+    "revision": "d16a66b8e200df026ff47176c780aca2",
+    "url": "/clicky-game/static/media/31-ice-king.w700.h700.d16a66b8.jpg"
+  },
+  {
+    "revision": "67f86d4a156d29363d1b56f782999be7",
+    "url": "/clicky-game/static/media/Baby_Pep-But.67f86d4a.jpg"
+  },
+  {
+    "revision": "dce13295f9c780d8ab11240f8a4ad817",
+    "url": "/clicky-game/static/media/LSP's_Phone.dce13295.png"
+  },
+  {
+    "revision": "664d03c198669f1453a8d2e1213918a1",
+    "url": "/clicky-game/static/media/RAIN.664d03c1.jpg"
+  },
+  {
+    "revision": "768ff486616a506a99474ac462d89ba8",
+    "url": "/clicky-game/static/media/S2e1_Marceline_And_Her_Ax_Bass.768ff486.png"
+  },
+  {
+    "revision": "51f8efeff91d3e1cf36f",
+    "url": "/clicky-game/static/js/main.51f8efef.chunk.js"
+  },
+  {
+    "revision": "14eaca096af62706ea2fb4476e53b15e",
+    "url": "/clicky-game/static/media/Sweet_Pea_Adventure_Time.14eaca09.png"
+  },
+  {
+    "revision": "0ec214f8b34d9960fb791b7007129262",
+    "url": "/clicky-game/static/media/Tree_runks.0ec214f8.jpg"
+  },
+  {
+    "revision": "0ae42b8ea688a09f0c06bbefa2c08eb1",
+    "url": "/clicky-game/static/media/betty2.0ae42b8e.jpg"
+  },
+  {
+    "revision": "2fd842d705b0ccf3e02d24fba3f8258d",
+    "url": "/clicky-game/static/media/bmo2.2fd842d7.png"
+  },
+  {
+    "revision": "7e7074ba7745aaac7c7a5c9030a4beaf",
+    "url": "/clicky-game/static/media/fern.7e7074ba.jpg"
+  },
+  {
+    "revision": "23df25ce809988e4af451d7dadf07d37",
+    "url": "/clicky-game/static/media/jake.23df25ce.jpg"
+  },
+  {
+    "revision": "ddae716750c247f61d6018b6670ed984",
+    "url": "/clicky-game/static/media/princess-bubblegum-icon-15.ddae7167.png"
+  },
+  {
+    "revision": "4611ef03f24c52f11cd4d718840b96bd",
+    "url": "/clicky-game/static/media/prismo_by_favouritefi-d5luv5v.4611ef03.png"
+  },
+  {
+    "revision": "5d08f13397a3a7fa665382c9e224b9dc",
+    "url": "/clicky-game/static/media/susan.5d08f133.png"
+  },
+  {
+    "revision": "c90a6c6a3f8cb5614fff07b1ca49dded",
+    "url": "/clicky-game/index.html"
+  }
+];
--- src/components/CharacterCard/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CharacterCard(props) {
-    return(
-        <div onClick={props.handleClick}>
-            <img src={props.image} alt={props.alt}/>
-        </div>
-    );
-}
